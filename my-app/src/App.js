848aef22c5d11da0047e95f6aa2ad37a750c56bc
unknown <stevenbigbangbom@gmail.com>
2023-02-28 20:07:16 +0700

    add BT1

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+function UserInfo(props) {
+  return <div>{props.user.name}</div>;
+}
+function Avatar(props) {
+  return <img src={props.user.avatarUrl} alt={props.user.name} />;
+}
+function Datee(props) {
+  return <div>{formatDate(props.date)}</div>;
+}
+function Text(props) {
+  return <div>{props.text}</div>;
+}
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "http://placekitten.com/g/64/64",
+  },
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Avatar user={comment.author} />
+      <UserInfo user={comment.author} />
+      <Text text={comment.text} />
+      <Datee date={comment.date} />
+    </div>
+  );
+}
+
+export default App;
